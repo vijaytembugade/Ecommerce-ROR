02d49c8d93ce881a5ce95d7529ed4c842415060e
vishal jain <vijay.tembugade21@gmail.com>
2021-12-02 16:45:09 +0530

    Catlog display

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -45,24 +45,24 @@ class ProductTest < ActiveSupport::TestCase
   #   end
   # end
 
-  test "product is not valid without a a unique title " do
-    product = Product.new(title:products(:ruby).title,
-      description: "yyy",
-      price: 1,
-      image_url: "fred.gif",
-    )
-  assert product.invalid?
-  assert_equal ["has already been taken"], product.errors[:title]
-  end
+  # test "product is not valid without a a unique title " do
+  #   product = Product.new(title:products(:ruby).title,
+  #     description: "yyy",
+  #     price: 1,
+  #     image_url: "fred.gif",
+  #   )
+  # assert product.invalid?
+  # assert_equal ["has already been taken"], product.errors[:title]
+  # end
 
-  test "product is not valid without a a unique title -i18n" do
-    product = Product.new(title:products(:ruby).title,
-      description: "yyy",
-      price: 1,
-      image_url: "fred.gif",
-    )
-  assert product.invalid?
-  assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
-  end
+  # test "product is not valid without a a unique title -i18n" do
+  #   product = Product.new(title:products(:ruby).title,
+  #     description: "yyy",
+  #     price: 1,
+  #     image_url: "fred.gif",
+  #   )
+  # assert product.invalid?
+  # assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
+  # end
       
 end
