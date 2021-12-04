0a89a2b88e4765e391d2f65319b9edbefbfaf312
vishal jain <vijay.tembugade21@gmail.com>
2021-12-04 10:47:35 +0530

    Javascript and Ajax

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,7 @@ class ProductsController < ApplicationController
 
   # GET /products or /products.json
   def index
-    @products = Product.all
+    @products = Product.all.order(:title)
   end
 
   # GET /products/1 or /products/1.json
@@ -41,6 +41,9 @@ class ProductsController < ApplicationController
       if @product.update(product_params)
         format.html { redirect_to @product, notice: "Product was successfully updated." }
         format.json { render :show, status: :ok, location: @product }
+        @products = Product.all.order(:title)
+        ActionCable.server.broadcast "products",
+                                     html: render_to_string("store/index", layout: false)
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @product.errors, status: :unprocessable_entity }
@@ -58,13 +61,14 @@ class ProductsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_product
-      @product = Product.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def product_params
-      params.require(:product).permit(:title, :description, :image_url, :price)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def product_params
+    params.require(:product).permit(:title, :description, :image_url, :price)
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def render_if(condition, record)
+    if condition
+      render record
+    end
+  end
+
+  def number_to_indian_currency(number)
+    if number.present?
+      string = number.to_s.split(".")
+      number = string[0].gsub(/(\d+)(\d{3})$/) { p = $2; "#{$1.reverse.gsub(/(\d{2})/, '\1,').reverse},#{p}" }
+      number = number.gsub(/^,/, "") + "." + string[1] if string[1] # remove leading comma
+      number = number[1..-1] if number[0] == 44
+    end
+    "Rs.#{number}"
+  end
 end
