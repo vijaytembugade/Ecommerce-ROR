41189bc29ff723c826e899a58ed0b5813a77c861
Vijay Tembugade <vijay.tembugade21@gmail.com>
2021-12-04 10:50:51 +0530

    javascript and ajax modified

---
+++ app/controllers/line_items_controller.rb
@@ -0,0 +1,86 @@
+class LineItemsController < ApplicationController
+  include CurrentCart
+  before_action :set_cart, only: [:create, :update]
+  before_action :set_line_item, only: %i[ show edit update destroy ]
+
+  # GET /line_items or /line_items.json
+  def index
+    @line_items = LineItem.all
+  end
+
+  # GET /line_items/1 or /line_items/1.json
+  def show
+  end
+
+  # GET /line_items/new
+  def new
+    @line_item = LineItem.new
+  end
+
+  # GET /line_items/1/edit
+  def edit
+  end
+
+  # POST /line_items or /line_items.json
+  def create
+    product = Product.find(params[:product_id])
+    @line_item = @cart.add_product(product)
+
+    respond_to do |format|
+      if @line_item.save
+        format.html { redirect_to store_index_url }
+        format.js { @current_item = @line_item }
+        format.json { render :show, status: :created, location: @line_item }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /line_items/1 or /line_items/1.json
+  def update
+    # product = Product.find(params[:product_id])
+    @line_item = LineItem.find(params[:id])
+
+    @line_item.quantity -= 1 if @line_item.quantity > 1
+
+    # @line_item.quantity -= 1
+
+    respond_to do |format|
+      if @line_item.save
+        format.html { redirect_to store_index_url, notice: "Cart is successfully updated." }
+        format.json { render :show, status: :ok, location: @line_item }
+        format.js { @current_item = @line_item }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /line_items/1 or /line_items/1.json
+  def destroy
+    @line_item.destroy
+    respond_to do |format|
+      format.html { redirect_to store_index_url, notice: "Line item was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  # def line_item_params
+  #   params.require(:line_item).permit(:product_id)
+  # end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_line_item
+    @line_item = LineItem.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def line_item_params
+    params.require(:line_item).permit(:product_id, :cart_id)
+  end
+end
